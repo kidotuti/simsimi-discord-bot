129376ba76c02c347d58988833a689110b413404
Vũ Văn Tiến <tienvu.061298@gmail.com>
2019-02-18 02:49:53 +0700

    Update simsimi.py

--- simsimi.py
+++ simsimi.py
@@ -21,8 +21,9 @@ async def on_ready():
     await bot.change_presence(game=discord.Game(name=",sim | ,simsimi | Bot được phát triển bởi Kido"), status=discord.Status('idle'))
 
 @bot.command()
-async def sim(ctx):
-    await ctx.send("Gọi gì bố?")
+async def help(ctx):
+    embed = discord.Embed(title="Simsimi", description="Đéo có lệnh nào cả. Kido hứa sẽ update sau", color=0xeee657)
+    await bot.say(embed=embed);
     
 @bot.command(aliases=['sim'])
 async def simsimi(*msg):
@@ -32,10 +33,35 @@ async def simsimi(*msg):
     response = requests.get(api, params=[("text", word)]).json()
     embed = discord.Embed(description="Không kết quả nào được tìm thấy!", colour=0x30d7e9)
     if len(response["messages"]) == 0:
-            return await client.say(embed=embed)
+            return await bot.say(embed=embed)
     embed=discord.Embed(title=response['messages'][0]['text'], color=embed.colour)
     embed.set_author(name="Simsimi", url="https://www.facebook.com/Kidokhongbigay", icon_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqSu3M-XrbptMiku7dAIKVQQMb8euR-5osgBLpxXkktdqGBcxu")
     await bot.say(embed=embed);
+    
+@bot.command(aliases=['ud'])
+async def urban(*msg):
+    """Tìm từ trong từ điển Urban."""
+    word = ' '.join(msg)
+    api = "http://api.urbandictionary.com/v0/define"
+    response = requests.get(api, params=[("term", word)]).json()
+    embed = discord.Embed(description="Không kết quả nào được tìm thấy!", colour=0xFF0000)
+    if len(response["list"]) == 0:
+        return await bot.say(embed=embed)
+    embed = discord.Embed(title="Từ cần tra nghĩa", description=word, colour=embed.colour)
+    embed.add_field(name="Định nghĩa hàng đầu:", value=response['list'][0]['definition'])
+    embed.add_field(name="Ví dụ:", value=response['list'][0]["example"])
+    embed.set_footer(text="Thẻ: " + ', '.join(response['tags']))
+    await bot.say(embed=embed)
    
+@bot.command(pass_context=True)
+async def hug(ctx, *, member: discord.Member = None):
+    """Ôm ai đó!"""
+    if member is None:
+            await client.say(ctx.message.author.mention + " đã được ôm!")
+    else:
+            if member.id == ctx.message.author.id:
+                await client.say(ctx.message.author.mention + " đã tự ôm chính mình!")
+            else:
+                await client.say(member.mention + " đã được ôm bởi " + ctx.message.author.mention + "!")
 
 bot.run(os.environ['BOT_TOKEN'])
