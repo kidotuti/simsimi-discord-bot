72bbe50368373cd4717ef8094d7bd006cff1b2b7
Vũ Văn Tiến <tienvu.061298@gmail.com>
2019-02-17 18:25:01 +0700

    Update simsimi.py

--- simsimi.py
+++ simsimi.py
@@ -29,6 +29,7 @@ async def simsimi(*msg):
         embed = discord.Embed(description="Không kết quả nào được tìm thấy!", colour=0xFF0000)
         embed = discord.Embed(title="Simsimi BOT", colour=embed.colour)
         embed.set_footer(text="Created by Kido. Have a great time!")
-        await bot.say(embed=embed)
-        
+        await bot.say(embed=embed);
+
+# Read client token from hihi
 bot.run(os.environ['BOT_TOKEN'])
