6fa1a1249fcf77deeca673d66689611860adc14b
Vũ Văn Tiến <tienvu.061298@gmail.com>
2019-02-17 15:42:24 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,15 +3,34 @@ const fetch = require('node-fetch');
 const querystring = require('querystring');
 
 const client = new Discord.Client();
+const prefix = ',';
 
 client.once('ready', () => {
 	console.log("Kido! I'm Ready!");
 });
 
-client.on('message', message => {
-	if (message.content === ${querystring}) {
-		const body = fetch.get('http://api.vietbot.net/simsimi.php?key=sibendz&text=${querystring}').then(response => response.json());
-		message.reply(body);
+client.on('message', async message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'simsimi') {
+		if (!args.length) {
+			return message.channel.send('Gọi gì bố?');
+		}
+
+		const query = querystring.stringify({ term: args.join(' ') });
+
+		const { body } = await fetch.get(`http://api.vietbot.net/simsimi.php?key=sibendz&text=${query}`).then(response => response.json());
+
+		if (!body.list.length) {
+			return message.channel.send(`Deoco từ này **${args.join(' ')}**, dạy bố đi!`);
+		}
+
+		const [answer] = body.list;
+
+		message.channel.send(body.list[0].definition);
 	}
 });
 
