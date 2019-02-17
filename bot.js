80f4fd9af589aef54072cbda0dfd67cb5e8d9823
Vũ Văn Tiến <tienvu.061298@gmail.com>
2019-02-17 14:22:10 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,38 +1,51 @@
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const querystring = require('querystring');
 
 const client = new Discord.Client();
+const prefix = '!';
 
- 
+const trim = (str, max) => (str.length > max ? `${str.slice(0, max - 3)}...` : str);
 
-client.on('ready', () => {
-
-    console.log('I am ready! Kido');
-
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
+client.on('message', async message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-client.on('ready', () => {
-    client.user.setStatus('dnd')
-    client.user.setPresence({
-        game: {
-            name: 'Simsimi BOT, developed by Kido#9203. Have a great time!',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/kidoooooooooooo"
-        }
-    });
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'cat') {
+		const { body } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+
+		message.channel.send(body.file);
+	} else if (command === 'urban') {
+		if (!args.length) {
+			return message.channel.send('You need to supply a search term!');
+		}
+
+		const query = querystring.stringify({ term: args.join(' ') });
+
+		const { body } = await fetch.get(`https://api.urbandictionary.com/v0/define${query}`).then(response => response.json());
+
+		if (!body.list.length) {
+			return message.channel.send(`No results found for **${args.join(' ')}**.`);
+		}
+
+		const [answer] = body.list;
+
+		const embed = new Discord.RichEmbed()
+			.setColor('#EFFF00')
+			.setTitle(answer.word)
+			.setURL(answer.permalink)
+			.addField('Definition', trim(answer.definition, 1024))
+			.addField('Example', trim(answer.example, 1024))
+			.addField('Rating', `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.`);
+
+		message.channel.send(embed);
+	}
 });
-
- 
-
-client.on('message', message => {
-
-    if (message.content === 'kido') {
-
-       message.reply('Bạn cần Kido giúp gì nào? Cứ ibox đừng ngại nhé!')
-
-       }
-
-});
-
 
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
