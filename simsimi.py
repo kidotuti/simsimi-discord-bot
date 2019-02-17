ac33ffed1ffe787a0df213bc000df06544808789
Vũ Văn Tiến <tienvu.061298@gmail.com>
2019-02-17 20:10:10 +0700

    Update simsimi.py

--- simsimi.py
+++ simsimi.py
@@ -7,7 +7,7 @@ import os
 
 def get_prefix(bot, msg):
     prefixes = [',']
-    me=['kido.']
+    me=['.']
     if msg.author.id == '321586138932445184':
         return commands.when_mentioned_or(*me)(bot, msg)
     return commands.when_mentioned_or(*prefixes)(bot, msg)
@@ -20,7 +20,7 @@ async def on_ready():
     print("Connecting...")
     
 @bot.command(aliases=['sim'])
-async def simsimi(*msg):
+async def sim(*msg):
     """Nói chuyện với Simsimi."""
     word = ' '.join(msg)
     api = "http://api.vietbot.net/simsimi.php?key=sibendz"
@@ -30,7 +30,6 @@ async def simsimi(*msg):
             return await client.say(embed=embed)
     embed=discord.Embed(title="Trả lời", description=response['messages'][0]['text'], color=embed.colour)
     embed.set_author(name="Simsimi", url="https://www.facebook.com/Kidokhongbigay", icon_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqSu3M-XrbptMiku7dAIKVQQMb8euR-5osgBLpxXkktdqGBcxu")
-    embed.set_thumbnail(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhhAGI4daSF2ejjSXoa_PiuzNSOosX_UxjgdLFLSJWX1MdnVgQ")
     embed.set_footer(text="Bot được phát triển bởi Kido!")
     await bot.say(embed=embed);
 
