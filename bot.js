666ef543c88c6ea06c6c1a84ab43b542d47fb983
Vũ Văn Tiến <tienvu.061298@gmail.com>
2019-02-17 16:03:03 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,22 +9,26 @@ client.once('ready', () => {
 	console.log("Kido! I'm Ready!");
 });
 
-client.on('message', message => {
+client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === 'simsimi') {
+	if (command === 'sim') {
 		if (!args.length) {
 			return message.channel.send('Gọi gì bố?');
 		}
 
 		const query = querystring.stringify({ term: args.join(' ') });
 
-		const { body } = fetch(`http://api.vietbot.net/simsimi.php?key=sibendz&text=${query}`).then(response => response.json());
+		const { body } = await fetch.get(`http://api.vietbot.net/simsimi.php?key=sibendz&text=${query}`).then(response => response.json());
 
-		message.channel.send(body);
+		const embed = new Discord.RichEmbed()
+			.setColor('#00ffff')
+			.setTitle('Simsimi BOT, created by Kido')
+
+		message.channel.send(embed);
 	}
 });
 
